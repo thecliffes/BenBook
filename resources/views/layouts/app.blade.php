838dc0e6ff8bb629b3b10041d47ebe0ebe6903a6
Ben <bencliffe1@gmail.com>
2023-01-04 14:05:56 +0000

    layout file code

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang = "en">
+    <head>
+        <title>@yield('title')</title>
+    </head>
+    
+    <body>
+        <h1>@yield('title')</h1>
+
+        <div>
+            @yield('content')
+        </div>
+    </body>
+</html>
+
