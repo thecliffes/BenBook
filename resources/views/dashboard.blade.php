518eae0e1b0da3b3876e375288bdc8d9655c6224
Ben <bencliffe1@gmail.com>
2023-01-05 16:22:05 +0000

    updated styling

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,22 +5,17 @@
         </h2>
     </x-slot>
 
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
+            <div class="p-6 text-gray-900">
+                <ul>
+                    @foreach ($users as $user)
+                        <li>{{ $user -> name }}</li>
+                    @endforeach
+                </ul>
             </div>
         </div>
-    </div>
-
-    <div class="p-6 text-gray-900">
-        <ul>
-            @foreach ($users as $user)
-                <li>{{ $user -> name }}</li>
-            @endforeach
-        </ul>
     </div>
         
 
