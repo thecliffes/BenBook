d0d429f2554552a10e8cca8ccdd08c536d2c70d5
Ben <bencliffe1@gmail.com>
2023-01-05 15:15:51 +0000

    added new post tab

---
+++ resources/views/newpost.blade.php
@@ -0,0 +1,11 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('New Post') }}
+        </h2>
+    </x-slot>
+
+
+    <p> Tester testtingingignign</p>
+
+</x-app-layout>
