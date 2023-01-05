db4b443f43edac783a2f8fb1d10960f99b6d780a
Ben <bencliffe1@gmail.com>
2023-01-05 12:17:59 +0000

    initial breeze code

---
+++ resources/views/layouts/basic.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang = "en">
+    <head>
+        <title>@yield('title')</title>
+    </head>
+    <body>
+        <h1>@yield('title')</h1>
+
+        <div>
+            @yield('content')
+        </div>
+
+</body>
+</html>
