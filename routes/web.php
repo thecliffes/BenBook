27af9777fc589f621808cc16d839faa51f783f30
Ben <bencliffe1@gmail.com>
2023-01-05 16:09:20 +0000

    added view to user controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,9 +19,8 @@ Route::get('/', function () {
     return view('users/home');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [UserController::class, 'index']
+)->middleware(['auth', 'verified'])->name('dashboard');;
 
 Route::get('/newpost', function () {
     return view('newpost');
